59c9a634875f71847663dae809d727eb1098c160
pahalagedara <thilinam4@gmail.com>
2024-12-08 20:04:54 +0530

    clean the code

--- src/module/tutor/controllers/tutorGetter.ts
+++ src/module/tutor/controllers/tutorGetter.ts
@@ -1,9 +1,7 @@
 import tutorService from "../tutor.service";
 
 const getTutors = async(page: number) => {
-    const dbTutor = await tutorService.findTutors(page);
-    console.log(dbTutor);
-    return  await tutorService.findTutors(page);
+    return await tutorService.findTutors(page);
 };
 
 export default {
