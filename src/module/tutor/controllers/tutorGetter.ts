0e80edc0066db7e37d20c1f97cc47f1ca60cc6da
pahalagedara <thilinam4@gmail.com>
2024-12-08 20:01:20 +0530

    Changed: subject array to tutor

--- src/module/tutor/controllers/tutorGetter.ts
+++ src/module/tutor/controllers/tutorGetter.ts
@@ -1,6 +1,8 @@
 import tutorService from "../tutor.service";
 
 const getTutors = async(page: number) => {
+    const dbTutor = await tutorService.findTutors(page);
+    console.log(dbTutor);
     return  await tutorService.findTutors(page);
 };
 
